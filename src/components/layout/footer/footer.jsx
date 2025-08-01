cd2dbed88fbcd184457db89e7f9d0f785956b3b1
Chackmoole <cebep-21@mail.ru>
2025-08-01 12:27:13 +0300

    добавлен компонент Text + Footer

--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -1,8 +1,10 @@
 import './footer-style.css';
 import { Logo } from '../../ui/logo/logo';
+import { Text, TextVariant } from '../../ui/text/text';
 
 export const Footer = () => { 
     return <footer className='footer'>
         <Logo />
+        <Text variant={TextVariant.normal}>Создано 2025</Text>
     </footer>
 }
