a19a1bebca032398dccff085e343de095815ce96
Chackmoole <cebep-21@mail.ru>
2025-08-04 16:14:54 +0300

    Стилизация блока features

--- src/components/blocks/features/features.jsx
+++ src/components/blocks/features/features.jsx
@@ -1,13 +1,20 @@
-import "./features-style.css";
 import { FeaturesCard } from "./features-card/features-card";
 import { PageWrapper } from "../../layout/page-wrapper/page-wrapper";
+import { Title, TitleVariants } from "../../ui/title/title";
+import { Button } from "../../ui/button/button";
+import "./features-style.css";
+
 export const Features = () => {
   return (
     <PageWrapper>
       <section className="features">
+        <Title variant={TitleVariants.h2} className="features__title">
+          Почему фермерские продукты лучше?
+        </Title>
         <ul className="features__list">
           <FeaturesCard />
         </ul>
+        <Button>Купить</Button>
       </section>
     </PageWrapper>
   );
