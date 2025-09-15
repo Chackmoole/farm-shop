7af90ad3c9da7c10fb262f5d02b7ee0b7792355e
chackmol <cebep-21@mail.ru>
2025-09-15 18:17:35 +0300

    Добавлен файл основной темы + установлен prettier

---
+++ src/theme/default.js
@@ -0,0 +1,18 @@
+export const defaultTheme = {
+  COLORS: {
+    colorBlackText: "#333333",
+    colorGray: "#f6f6f6",
+    colorWhite: "#ffffff",
+    colorLightBlue: "#d8ecfe",
+    colorLightGreen: "#e1edce",
+    colorGreen: "#88aa4d",
+    colorLigthPink: "#f8ddd7",
+    colorDarkOrange: "#f75531",
+    colorOrange: "#fc9b27",
+    colorAcitveOrange: "#fc7427",
+  },
+  FONTS: {
+    fontFamily: '"Inter", "Arial", sans-serif',
+    fontSizeDefault: "18px",
+  },
+};
