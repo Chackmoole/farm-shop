7942e2d454d81eab184c16fce3a77b8b003013af
chackmol <cebep-21@mail.ru>
2025-09-16 11:17:56 +0300

    Добавлен ThemeProvider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { GlobalStyle } from "./styles";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./theme/default";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
