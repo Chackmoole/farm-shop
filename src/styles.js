83912e6f306412a6c245ce3584f767e4c7f389b6
chackmol <cebep-21@mail.ru>
2025-09-16 11:06:08 +0300

    Добавлен styles.js

---
+++ src/styles.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`html {
+  height: 100%;
+}
+body,
+html {
+  margin: 0;
+}
+body {
+  position: relative;
+  min-height: 100%;
+  font-family: ${(props) => props.theme.FONTS.colorBlackText};
+  font-size: ${(props) => props.theme.FONTS.fontSizeDefault};
+  font-weight: 400;
+  color: ${(props) => props.theme.COLORS.colorBlackText};
+}
+.visually-hidden {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  border: 0;
+  padding: 0;
+  white-space: nowrap;
+  -webkit-clip-path: inset(100%);
+  clip-path: inset(100%);
+  clip: rect(0 0 0 0);
+  overflow: hidden;
+}
+`;
