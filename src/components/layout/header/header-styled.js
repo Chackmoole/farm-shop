bfa9280040e71d7053d45804c6f91a92908102bc
chackmol <cebep-21@mail.ru>
2025-09-24 13:22:01 +0300

    Стилизация блока Header styled

--- src/components/layout/header/header-styled.js
+++ src/components/layout/header/header-styled.js
@@ -1,6 +1,8 @@
-.header {
+import styled from "styled-components";
+
+export const StyledStyled = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
   min-height: 80px;
-}
+`;
--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -1,15 +1,15 @@
-import "./header-style.css";
 import { Logo } from "../../ui/logo/logo";
 import { Button } from "../../ui/button/button";
 import { PageWrapper } from "../page-wrapper/page-wrapper";
+import { StyledStyled } from "./header-styled";
 
 export const Header = () => {
   return (
     <PageWrapper>
-      <header className="header">
+      <StyledStyled>
         <Logo />
         <Button>Купить</Button>
-      </header>
+      </StyledStyled>
     </PageWrapper>
   );
 };
