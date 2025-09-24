5cb9ee968b6dc66c9e4a6a33541c2c9ac34dbd77
chackmol <cebep-21@mail.ru>
2025-09-24 12:57:24 +0300

    Стилизация блока Footer styled.

---
+++ src/components/layout/footer/footer-styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 80px;
+  margin-top: auto;
+`;
--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -2,14 +2,15 @@ import "./footer-style.css";
 import { Logo } from "../../ui/logo/logo";
 import { Text, defaultVariantMapping } from "../../ui/text/text";
 import { PageWrapper } from "../page-wrapper/page-wrapper";
+import { StyledFooter } from "./footer-styled";
 
 export const Footer = () => {
   return (
-    <PageWrapper className="footer-wrapper">
-      <footer className="footer">
+    <PageWrapper>
+      <StyledFooter>
         <Logo />
         <Text variant={defaultVariantMapping.body1}>Создано 2025</Text>
-      </footer>
+      </StyledFooter>
     </PageWrapper>
   );
 };
