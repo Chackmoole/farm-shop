416b794f0d292cc965a874e2cae7e57892acf667
chackmol <cebep-21@mail.ru>
2025-11-22 19:55:50 +0300

    Интегрирована логика вывода в alert id-шников заказа и адреса. А так же очищения поля адреса после отправки формы.

--- src/components/blocks/order-page/order-purchase-form/order-purchase-form.jsx
+++ src/components/blocks/order-page/order-purchase-form/order-purchase-form.jsx
@@ -3,6 +3,7 @@ import { OrderFilter } from "../order-filter/order-filter";
 import { products } from "../../../../mock/products-data";
 import { OrderFilterItem } from "../order-filter-item/order-filter-item";
 import { OrderPurchase } from "../order-purchase/order-purchase";
+import { useState } from "react";
 
 export const OrderPurchaseForm = ({
   handelOnFilterChange,
@@ -11,6 +12,18 @@ export const OrderPurchaseForm = ({
 }) => {
   const isAvailable = activeProducts.length > 0;
 
+  const [address, setAddress] = useState("");
+  const handleOnChange = (e) => {
+    setAddress(e.target.value);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    alert(
+      `Заказаны продукты с id ${activeProducts} и адресом доставки ${address}`
+    );
+    setAddress("");
+  };
   return (
     <StyledOrderPurchaseForm>
       <OrderFilter>
@@ -27,7 +40,13 @@ export const OrderPurchaseForm = ({
           );
         })}
       </OrderFilter>
-      <OrderPurchase price={price} isAvailable={isAvailable} />
+      <OrderPurchase
+        price={price}
+        isAvailable={isAvailable}
+        onClick={handleOnSubmit}
+        onChange={handleOnChange}
+        inputValue={address}
+      />
     </StyledOrderPurchaseForm>
   );
 };
--- src/components/blocks/order-page/order-purchase/order-purchase.jsx
+++ src/components/blocks/order-page/order-purchase/order-purchase.jsx
@@ -5,20 +5,30 @@ import {
 } from "./order-purchase-styled";
 import { Text } from "../../../ui/text/text";
 
-export const OrderPurchase = ({ price, isAvailable }) => {
+export const OrderPurchase = ({
+  price,
+  isAvailable,
+  onClick,
+  onChange,
+  inputValue,
+}) => {
   return (
     <StyledOrderPurchase>
       <Text variant="body2" mb="24px">
         Сделать заказ
       </Text>
-      <StyledOrderAddressInput placeholder="Введите адрес доставки" />
+      <StyledOrderAddressInput
+        placeholder="Введите адрес доставки"
+        onChange={onChange}
+        value={inputValue}
+      />
       <Text variant="caption1" mb="8px">
         Цена
       </Text>
       <Text variant="h4" mb="32px">
         {price} руб.
       </Text>
-      <StyledOrderOrderBuyButton disabled={!isAvailable}>
+      <StyledOrderOrderBuyButton disabled={!isAvailable} onClick={onClick}>
         Купить
       </StyledOrderOrderBuyButton>
     </StyledOrderPurchase>
