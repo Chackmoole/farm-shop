e0f82b96175ef3a2bf124b9cbb34010a58425214
chackmol <cebep-21@mail.ru>
2025-11-25 12:48:03 +0300

    Добавлен UseNavigate()

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -2,9 +2,10 @@ import { Logo } from "../../ui/logo/logo";
 import { Button } from "../../ui/button/button";
 import { PageWrapper } from "../page-wrapper/page-wrapper";
 import { StyledNav, StyledStyled } from "./header-styled";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 export const Header = () => {
+  let navigate = useNavigate();
   return (
     <PageWrapper>
       <StyledStyled>
@@ -14,7 +15,7 @@ export const Header = () => {
           <Link to="/order">Заказ</Link>
         </StyledNav>
 
-        <Button>Купить</Button>
+        <Button onClick={() => navigate("/order")}>Купить</Button>
       </StyledStyled>
     </PageWrapper>
   );
