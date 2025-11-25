ce6cd33660ad7454bdfea40433d0266fed5428d6
chackmol <cebep-21@mail.ru>
2025-11-25 12:01:23 +0300

    Добавлен и настроен блок простой навигации в header

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -1,13 +1,19 @@
 import { Logo } from "../../ui/logo/logo";
 import { Button } from "../../ui/button/button";
 import { PageWrapper } from "../page-wrapper/page-wrapper";
-import { StyledStyled } from "./header-styled";
+import { StyledNav, StyledStyled } from "./header-styled";
+import { Link } from "react-router";
 
 export const Header = () => {
   return (
     <PageWrapper>
       <StyledStyled>
         <Logo />
+        <StyledNav>
+          <Link to="/">Главная</Link>
+          <Link to="/order">Заказ</Link>
+        </StyledNav>
+
         <Button>Купить</Button>
       </StyledStyled>
     </PageWrapper>
